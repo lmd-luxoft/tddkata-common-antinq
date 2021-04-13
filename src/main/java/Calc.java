e064029f522a6a230c9cdcd670df18dd1e0fbf38
antinq <tylor@ukr.net>
2021-04-13 12:34:28 +0300

    Add Calc.java and appropriate test

---
+++ src/main/java/Calc.java
@@ -0,0 +1,8 @@
+import sun.reflect.generics.reflectiveObjects.NotImplementedException;
+
+public class Calc {
+    public String sum(String expression){
+        throw new NotImplementedException();
+
+    }
+}
---
+++ src/test/java/CalcTest.java
@@ -0,0 +1,11 @@
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.fail;
+
+public class CalcTest {
+
+    @Test
+    public void sumTest(){
+        fail("nit yet implemented");
+    }
+}
