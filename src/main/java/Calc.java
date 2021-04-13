dda0e820a14f08ae11fa19048877986861c25a0c
antinq <tylor@ukr.net>
2021-04-13 13:15:08 +0300

    Add Tests

--- src/main/java/Calc.java
+++ src/main/java/Calc.java
@@ -1,8 +1,8 @@
 import sun.reflect.generics.reflectiveObjects.NotImplementedException;
 
 public class Calc {
-    public String sum(String expression){
-        throw new NotImplementedException();
 
+    public int sum(String expression){
+        throw new NotImplementedException();
     }
 }
--- src/test/java/CalcTest.java
+++ src/test/java/CalcTest.java
@@ -1,11 +1,43 @@
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
-import static org.junit.jupiter.api.Assertions.fail;
+import static org.junit.jupiter.api.Assertions.*;
 
 public class CalcTest {
 
-    @Test
-    public void sumTest(){
-        fail("nit yet implemented");
+    public Calc calc;
+
+    @BeforeEach
+    public void preconditions(){
+        calc = new Calc();
     }
+
+    @Test
+    public void zeroNumbersSumTest(){
+        assertEquals(0, calc.sum(""));
+    }
+
+
+    @Test
+    public void oneNumberSumTest(){
+        assertEquals(5, calc.sum("5"));
+    }
+
+    @Test
+    public void oneNumberAndComaSumTest(){
+        assertEquals(8, calc.sum("8,"));
+    }
+
+    @Test
+    public void twoNumbersSumTest(){
+        assertEquals(12, calc.sum("6,5"));
+    }
+
+    @Test()
+    public void incorrectInputDataSumTest(){
+        Exception exception = assertThrows(Exception.class, () ->
+                calc.sum("xx,l"));
+        assertEquals("Incorrect input data", exception);
+    }
+
 }
